204f4f23685f32728125941ba415969765c51116
Nick <nicolas.lb@outlook.com>
2019-05-15 10:08:06 -0400

    continuation de la shapesolution

--- ShapeSolution.h
+++ ShapeSolution.h
@@ -1,11 +1,20 @@
 #ifndef SHAPESOLUTION.H
 #define SHAPESOLUTION.H
+#include "Solution.h"
+#include "Obstacle.h"
 
-class ShapeSolution
+class ShapeSolution extends Solution
 {
 public:
 	ShapeSolution();
 	~ShapeSolution();
+	virtual void draw();
+
+private:
+	virtual bool collide(Obstacke const & obs);
+	virtual double distance(Obstacle const & obs);
+	virtual double area();
+	virtual double perimeter();
 };
 
 #endif // SHAPESOLUTION.H
