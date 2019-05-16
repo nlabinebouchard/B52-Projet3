3cb437ca2345aafe7a17fcb3188eb5650f4fe7fd
1575290 <1575290@VM70035073>
2019-05-16 11:26:25 -0400

    console OPPPP

--- Console/main.cpp
+++ Console/main.cpp
@@ -1,10 +1,28 @@
 #include "Console.h"
+#include "ConsoleContext.h"
+#include <iostream>
 
 
 int main()
 {
-	ConsoleContext themeCompact();
+	
+	ConsoleContext myContext(50,50,"Projet-3 B52",20,20, L"Consolas");
 
+	Console::defineContext(myContext);
+
+	//ConsoleImage::drawLine(20, 80, 20, 60, 'b', ConsoleColor::bb);
+
+	//Console::getInstance(); // necessaire pour re size la console de output
+
+	// ConsoleImage image;
+
+	//Console::getInstance().writer().randomize();
+
+	Console::getInstance().writer().createImage("Allo",'a').drawRect(1, 1, 10, 10, 'w', ConsoleColor::tb);
+
+	Console::getInstance().writer().write("Allo");
+
+	// Console::getInstance().writer().write();
 
 
 	int a{ 0 };
