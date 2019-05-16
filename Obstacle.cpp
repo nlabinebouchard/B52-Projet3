5c5626ddc879047ec97f24c74e612d5d7a961fb2
1575290 <1575290@VM70035073>
2019-05-16 09:21:32 -0400

    modif random et fix bug

--- Obstacle.cpp
+++ Obstacle.cpp
@@ -1,21 +1,18 @@
 #include "Obstacle.h"
 
 Obstacle::Obstacle(size_t const minX, size_t const minY, size_t const maxX, size_t const  maxY)
-	:mMinX{ minX }, mMinY{ minY }, mMaxX{ maxX }, mMaxY{ maxY }
+	
 {
 
-}
-
-Obstacle::~Obstacle()
-{
-	mMinX = mMinY = mMaxX = mMaxY = 0;
 }
 
 void Obstacle::randomizePosition(size_t minX, size_t minY, size_t maxX, size_t maxY)
 {
-	mMinX = minX;
-	mMinY = minY;
-	mMaxX = maxX;
-	mMaxY = maxY;
+
+
+
+
+
+
 
 }
